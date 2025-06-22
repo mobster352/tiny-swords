2a8c3f6161443f964544585280852f250fc0fa5a
mobster352 <hubertwalton352@gmail.com>
2025-06-22 00:07:13 -0400

    first sprite!

---
+++ include/constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SPRITE_SIZE 192
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,19 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "constants.h"
+
+/*
+* Parameters:
+* int xIndex: the horizontal index of the sprite sheet
+* int yIndex: the vertical index of the sprite sheet
+*
+* Returns:
+* Vector2: the position of the sprite as a Vector2
+*/
+Vector2 findSprite(int xIndex, int yIndex){
+	return (Vector2){xIndex * SPRITE_SIZE, yIndex * SPRITE_SIZE};
+}
 
 int main ()
 {
@@ -22,8 +26,18 @@ int main ()
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	SetTargetFPS(10); //the sprites were made to be 10 fps / 100ms
+
+
+
+	// Assuming you have a texture called 'atlas' loaded
+	Texture atlas = LoadTexture("Warrior_Blue.png");
+	Rectangle sourceRec = { 0, 0, 192, 192 }; // Example: First sprite in the sheet
+	Rectangle destRec = { 100, 100, 192, 192 }; // Example: Draw at (100, 100)
+	Vector2 origin = { 0.0f, 0.0f }; // Optional origin point
+
+	int xIndex = 0;
+	int yIndex = 0;
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -34,11 +48,19 @@ int main ()
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		// Draw the sprite
+		Vector2 sprite = findSprite(xIndex, yIndex);
+		sourceRec.x = sprite.x;
+		sourceRec.y = sprite.y;
+		DrawTexturePro(atlas, sourceRec, destRec, origin, 0.0f, WHITE);
+		xIndex++;
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		if(xIndex > 6){
+			xIndex = 0;
+			yIndex++;
+			if(yIndex > 8)
+				yIndex = 0;
+		}
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -46,7 +68,7 @@ int main ()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	UnloadTexture(atlas);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
